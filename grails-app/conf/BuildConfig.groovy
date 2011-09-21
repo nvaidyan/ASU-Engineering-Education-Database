27c7222111034f88346f6acdcf10ad213406ba96
Nicholas Vaidyanathan <visionary.software.solutions@gmail.com>
2011-09-21 03:12:14 -0700

    did stuff

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -3,7 +3,8 @@ grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
-
+        //changed port number for development
+        grails.server.port.http = 7272
 grails.project.dependency.resolution = {
     // inherit Grails' default dependencies
     inherits("global") {
